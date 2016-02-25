433f9f8bed82b63ac6d92bae5e13a84d3c3f61fd
Eric Kryski <e.kryski@gmail.com>
2016-02-25 10:23:48 -0700

    lowercasing usernames

--- examples/chat/src/services/user/user-model.js
+++ examples/chat/src/services/user/user-model.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   online: { type: Boolean, 'default': false },
   photoURL: { type: String, 'default': 'https://distillery.s3.amazonaws.com/profiles/anonymousUser.jpg' },
